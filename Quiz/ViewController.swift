9286871fc086d033edba2f73f02fff89b9893263
asrbekmahmudov <asrbekmahmudov7@gmail.com>
2024-07-15 09:52:41 +0500

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Asrbek Mahmudov on 15/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
