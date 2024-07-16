1c6a2b2b6a92969b36c575c6889c3fe0353c43c3
asrbekmahmudov <asrbekmahmudov7@gmail.com>
2024-07-16 13:50:04 +0500

    Project setted up

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -1,19 +1,41 @@
-//
-//  ViewController.swift
-//  Quiz
-//
-//  Created by Asrbek Mahmudov on 15/07/24.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7*7?",
+        "What is the capital of China?",
+        "What is beer made from?"
+    ]
+    let answers: [String] = [
+        "49",
+        "Beijing",
+        "Barley, water, hops, and yeast"
+    ]
+    var currentQuestionIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0 // If questions are over then goes back to the first question
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
 }
 
